3034397281912b6d61d860862814b7363a8190fe
Shravan <shravanbobade25@gmail.com>
2024-02-05 22:46:52 +0530

    initial success

--- index.js
+++ index.js
@@ -1,31 +1,47 @@
-var iterations = 3;
+var level = 0;
+var score = 0;
+var GamePattern = [];
+var userClicked = [];
 
-var keyGenerated = [];
+var GameOver = false;
 
-$("button").on("click", function () {
+$(document).on("keypress", function () {
   randomKeyGenerator();
-  //   var key = $(this);
-  //   console.log(key.html());
-  //   key.addClass("pressed");
-  //   setTimeout(function () {
-  //     key.removeClass("pressed");
-  //   }, 300);
-  //   keyGenerated.push(key.html());
 });
 
-function isSameKeyPressed(key) {}
+
+  $("button").on("click", function () {
+    key = $(this);
+    userClicked.push(key.html());
+
+    key.addClass("pressed");
+    setTimeout(function () {
+        key.removeClass("pressed");
+    },300);
+
+    isSameKeyPressed(userClicked.length - 1);
+
+  });
+
+function isSameKeyPressed(level) {
+    if(userClicked[level] === GamePattern[level]){
+        if(userClicked.length === GamePattern.length){
+            setTimeout(function(){
+                randomKeyGenerator();
+            },500);
+        }
+    }else{
+        $("h1").html("Game Over").addClass("Game-Over");
+    }
+}
 
 function randomKeyGenerator() {
-  while (iterations != 0) {
-      var key = $("button").eq(RandomNumberGenerator());
-      key.addClass("pressed");
-      setTimeout(function () {
-        key.removeClass("pressed");
-      }, 300);
-      keyGenerated.push(key.html());
-      iterations--;
-      
-  }
+  userClicked = [];
+  var key = $("button").eq(RandomNumberGenerator());
+  key.fadeIn(200).fadeOut(200).fadeIn(200);
+  GamePattern.push(key.html());
+  level++;
+  $("h1").html("Level "+ level);
 }
 
 function RandomNumberGenerator() {
